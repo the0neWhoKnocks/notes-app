bd1cf7703419c3a2a8426e18b8a81fd77758eb94
Trevor Lemon <duhlemman@gmail.com>
2021-07-11 13:56:30 -0700

    fix: log names are truncated

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,4 +1,19 @@
-require('ulog');
+if (!process.env.FOR_CLIENT_BUNDLE) {
+  process.env.log_format = 'lvl noPadName message';
+}
+
+const ulog = require('ulog');
+ulog.use({
+  use: [ require('ulog/mods/formats') ],
+  formats: {
+    noPadName: () => {
+      const fmt = (rec) => rec.name;
+      fmt.color = 'logger';
+      return fmt;
+    },
+  },
+});
+
 const { NAMESPACE__LOGGER } = require('../constants');
 const aL = require('anylogger');
 const rootLogger = aL(NAMESPACE__LOGGER);
