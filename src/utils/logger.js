a96385eaf063b20d4599691ff2d669dd9fb8abde
Nox <the0neWhoKnocks@users.noreply.github.com>
2021-07-31 13:00:12 -0700

    fix: ulog
    There were a couple issues (both possibly related to a Chrome update):
    1. Color function definitions were being output instead of the log type and namespace. Fixed it by using a custom `config.log_format`.
    2. Logs weren't always showing up unless I set the log level via a query param. Used to be able to just set a value in `localStorage`. Fixed it by setting `config.log`.

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,8 +1,16 @@
-if (!process.env.FOR_CLIENT_BUNDLE) {
-  process.env.log_format = 'lvl noPadName message';
-}
-
 const ulog = require('ulog');
+// NOTE: Overriding for both Server and Client because of the noted issues.
+// - If the Client issue ever gets fixed, the Server only logic for
+//   `process.env.FOR_CLIENT_BUNDLE` will have to be reimplemented.
+// Issues:
+// - Client: https://github.com/Download/ulog/issues/69
+// - Format padding: https://github.com/Download/ulog/issues/68
+ulog.config.log_format = 'lvl noPadName message';
+if (process.env.FOR_CLIENT_BUNDLE) {
+  ulog.config.log = 'debug';
+  window.localStorage.log = ulog.config.log;
+  window.localStorage.log_format = ulog.config.log_format;
+}
 ulog.use({
   use: [ require('ulog/mods/formats') ],
   formats: {
