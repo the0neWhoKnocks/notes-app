5eaa56b373138afc6c9333e470ad8bf012810176
Nox <the0neWhoKnocks@users.noreply.github.com>
2021-08-07 11:06:23 -0700

    ops: Created modifyUserData util
    This way I can keep Server and SW logic in sync.

--- src/server/api/user.data.set.js
+++ src/server/api/user.data.set.js
@@ -1,169 +1,25 @@
 const { writeFile } = require('fs');
-const { GROUP_NODE_SHAPE } = require('../../constants');
-const getPathNode = require('../../utils/getPathNode');
-const kebabCase = require('../../utils/kebabCase');
 const log = require('../../utils/logger')('api.user.data.set');
+const modifyUserData = require('../../utils/modifyUserData');
 const encrypt = require('../utils/encrypt');
 const getUserDataPath = require('../utils/getUserDataPath');
 const loadUserData = require('../utils/loadUserData');
 
-const sanitizeContent = (txt) => {
-  // While editing on the Client, only newline characters are present, but
-  // during the submission proccess (either via the Form or Post), carriage
-  // return characters are added as well, which messes with diff checking on the
-  // Client.
-  return txt.replace(/\r\n/g, '\n');
-};
-
 module.exports = async function setData(req, res) {
-  const {
-    appConfig,
-    body: {
-      action,
-      content,
-      id,
-      name,
-      oldName,
-      oldTitle,
-      password,
-      path,
-      prefs,
-      title,
-      type,
-      username,
+  const { appConfig, body: reqBody } = req;
+  const { password, username } = reqBody;
+  
+  const { data, error, logMsg } = await modifyUserData({
+    loadCurrentData: async () => {
+      return await loadUserData(appConfig, username, password);
     },
-  } = req;
+    logMsg: 'Data set',
+    reqBody,
+  });
   
-  if (!action) return res.error(400, 'Missing `action`');
-  else if (!['add', 'edit', 'delete'].includes(action)) return res.error(400, `The \`action\` "${action}" is unknown`);
-  else if (!type) return res.error(400, 'Missing `type`');
-  else if (!username && !password) return res.error(400, 'Missing `username` and `password`');
-  else if (!username) return res.error(400, 'Missing `username`');
-  else if (!password) return res.error(400, 'Missing `password`');
-  else if (!['group', 'note', 'preferences'].includes(type)) return res.error(400, `The \`type\` "${type}" is unknown`);
-  else if (type === 'note') {
-    let required = ['path', 'title'];
-    if (action === 'edit') required.push('oldTitle');
-    else if (action === 'delete') required = ['id', 'path'];
-    
-    const missingItems = required.filter(prop => !req.body[prop]).map(prop => `\`${prop}\``).join(' and ');
-    if (missingItems.length) return res.error(400, `Missing ${missingItems}`);
-  }
-  else if (type === 'group') {
-    let required = ['path', 'name'];
-    if (action === 'edit') required.push('oldName');
-    else if (action === 'delete') required = ['id', 'path'];
-    
-    const missingItems = required.filter(prop => !req.body[prop]).map(prop => `\`${prop}\``).join(' and ');
-    if (missingItems.length) return res.error(400, `Missing ${missingItems}`);
-  }
-  else if (type === 'preferences') {
-    const required = ['prefs'];
-    const missingItems = required.filter(prop => !req.body[prop]).map(prop => `\`${prop}\``).join(' and ');
-    if (missingItems.length) return res.error(400, `Missing ${missingItems}`);
-  }
-  
-  const data = await loadUserData(appConfig, username, password);
-  const notesData = data.notesData;
-  let logMsg = 'Data set';
-  
-  if (action === 'add') {
-    if (type === 'note') {
-      const { notes } = getPathNode(notesData, path);
-      const nodeId = kebabCase(title);
-      
-      if (!notes[nodeId]) {
-        notes[nodeId] = {
-          content: sanitizeContent(content) || '',
-          created: Date.now(),
-          title,
-        };
-        
-        logMsg = `Created note "${title}" in "${path}"`;
-      }
-      else return res.error(400, `Note with title "${title}" already exists in "${path}"`);
-    }
-    else if (type === 'group') {
-      const { groups } = getPathNode(notesData, path);
-      const nodeId = kebabCase(name);
-      
-      if (!groups[nodeId]) {
-        groups[nodeId] = { ...GROUP_NODE_SHAPE, groupName: name };
-        
-        logMsg = `Created group "${name}" in "${path}"`;
-      }
-      else return res.error(400, `The group "${name}" already exists in "${path}"`);
-    }
-  }
-  else if (action === 'edit') {
-    if (type === 'note') {
-      const { notes } = getPathNode(notesData, path);
-      let nodeId = kebabCase(oldTitle);
-      const oldNote = { ...notes[nodeId] };
-      
-      if (oldTitle !== title) {
-        const newNodeId = kebabCase(title);
-        if (notes[newNodeId]) {
-          return res.error(400, `Note with title "${title}" already exists in "${path}"`);
-        }
-        
-        delete notes[nodeId];
-        nodeId = newNodeId;
-      }
-      
-      notes[nodeId] = {
-        ...oldNote,
-        content: sanitizeContent(content) || '',
-        modified: Date.now(),
-        title,
-      };
-      
-      logMsg = `Updated note "${title}" in "${path}"`;
-    }
-    else if (type === 'group') {
-      const { groups } = getPathNode(notesData, path);
-      let nodeId = kebabCase(oldName);
-      const groupCopy = { ...groups[nodeId] };
-      
-      if (oldName !== name) {
-        const newNodeId = kebabCase(name);
-        if (groups[newNodeId]) {
-          return res.error(400, `Group "${name}" already exists in "${path}"`);
-        }
-        
-        delete groups[nodeId];
-        nodeId = newNodeId;
-      }
-      
-      groups[nodeId] = {
-        ...groupCopy,
-        groupName: name,
-      };
-      
-      logMsg = `Renamed group from "${oldName}" to "${name}" in "${path}"`;
-    }
-    else if (type === 'preferences') {
-      data.preferences = {
-        ...data.preferences,
-        ...prefs,
-      };
-      
-      logMsg = `Updated preferences for ${Object.keys(prefs).map(pref => `"${pref}"`).join(', ')}`;
-    }
-  }
-  else if (action === 'delete') {
-    if (type === 'note') {
-      const { notes } = getPathNode(notesData, path);
-      delete notes[id];
-      
-      logMsg = `Removed note "${id}" from "${path}"`;
-    }
-    else if (type === 'group') {
-      const { groups } = getPathNode(notesData, path);
-      delete groups[id];
-      
-      logMsg = `Removed group "${id}" from "${path}"`;
-    }
+  if (error) {
+    const { code, msg } = error;
+    return res.error(code, msg);
   }
   
   const { valueHex: encryptedUsername } = await encrypt(appConfig, username);
---
+++ src/utils/modifyUserData.js
@@ -0,0 +1,172 @@
+const { GROUP_NODE_SHAPE } = require('../constants');
+const getPathNode = require('./getPathNode');
+const kebabCase = require('./kebabCase');
+
+const getMissingRequiredItems = (required, propObj) => {
+  return required
+    .filter(prop => !propObj[prop])
+    .map(prop => `\`${prop}\``)
+    .join(' and ');
+};
+
+const sanitizeContent = (txt) => {
+  // While editing on the Client, only newline characters are present, but
+  // during the submission proccess (either via the Form or Post), carriage
+  // return characters are added as well, which messes with diff checking on the
+  // Client.
+  return txt.replace(/\r\n/g, '\n');
+};
+
+module.exports = async function modifyUserData({
+  loadCurrentData,
+  reqBody,
+}) {
+  const {
+    action,
+    content,
+    id,
+    name,
+    oldName,
+    oldTitle,
+    password,
+    path,
+    prefs,
+    title,
+    type,
+    username,
+  } = reqBody;
+  let missingRequiredItems;
+  
+  if (!action) return { error: { code: 400, msg: 'Missing `action`' } };
+  else if (!['add', 'edit', 'delete'].includes(action)) return { error: { code: 400, msg: `The \`action\` "${action}" is unknown` } };
+  else if (!type) return { error: { code: 400, msg: 'Missing `type`' } };
+  else if (!username && !password) return { error: { code: 400, msg: 'Missing `username` and `password`' } };
+  else if (!username) return { error: { code: 400, msg: 'Missing `username`' } };
+  else if (!password) return { error: { code: 400, msg: 'Missing `password`' } };
+  else if (!['group', 'note', 'preferences'].includes(type)) return { error: { code: 400, msg: `The \`type\` "${type}" is unknown` } };
+  else if (type === 'note') {
+    let required = ['path', 'title'];
+    if (action === 'edit') required.push('oldTitle');
+    else if (action === 'delete') required = ['id', 'path'];
+    
+    missingRequiredItems = getMissingRequiredItems(required, reqBody);
+  }
+  else if (type === 'group') {
+    let required = ['path', 'name'];
+    if (action === 'edit') required.push('oldName');
+    else if (action === 'delete') required = ['id', 'path'];
+    
+    missingRequiredItems = getMissingRequiredItems(required, reqBody);
+  }
+  else if (type === 'preferences') {
+    const required = ['prefs'];
+    missingRequiredItems = getMissingRequiredItems(required, reqBody);
+  }
+  
+  if (missingRequiredItems) return { error: { code: 400, msg: `Missing ${missingRequiredItems}` } };
+  
+  const data = await loadCurrentData();
+  const notesData = data.notesData;
+  let logMsg = 'Data set';
+  
+  if (action === 'add') {
+    if (type === 'note') {
+      const { notes } = getPathNode(notesData, path);
+      const nodeId = kebabCase(title);
+      
+      if (!notes[nodeId]) {
+        notes[nodeId] = {
+          content: sanitizeContent(content) || '',
+          created: Date.now(),
+          title,
+        };
+        
+        logMsg = `Created note "${title}" in "${path}"`;
+      }
+      else return { error: { code: 400, msg: `Note with title "${title}" already exists in "${path}"` } };
+    }
+    else if (type === 'group') {
+      const { groups } = getPathNode(notesData, path);
+      const nodeId = kebabCase(name);
+      
+      if (!groups[nodeId]) {
+        groups[nodeId] = { ...GROUP_NODE_SHAPE, groupName: name };
+        
+        logMsg = `Created group "${name}" in "${path}"`;
+      }
+      else return { error: { code: 400, msg: `The group "${name}" already exists in "${path}"` } };
+    }
+  }
+  else if (action === 'edit') {
+    if (type === 'note') {
+      const { notes } = getPathNode(notesData, path);
+      let nodeId = kebabCase(oldTitle);
+      const oldNote = { ...notes[nodeId] };
+      
+      if (oldTitle !== title) {
+        const newNodeId = kebabCase(title);
+        if (notes[newNodeId]) {
+          return { error: { code: 400, msg: `Note with title "${title}" already exists in "${path}"` } };
+        }
+        
+        delete notes[nodeId];
+        nodeId = newNodeId;
+      }
+      
+      notes[nodeId] = {
+        ...oldNote,
+        content: sanitizeContent(content) || '',
+        modified: Date.now(),
+        title,
+      };
+      
+      logMsg = `Updated note "${title}" in "${path}"`;
+    }
+    else if (type === 'group') {
+      const { groups } = getPathNode(notesData, path);
+      let nodeId = kebabCase(oldName);
+      const groupCopy = { ...groups[nodeId] };
+      
+      if (oldName !== name) {
+        const newNodeId = kebabCase(name);
+        if (groups[newNodeId]) {
+          return { error: { code: 400, msg: `Group "${name}" already exists in "${path}"` } };
+        }
+        
+        delete groups[nodeId];
+        nodeId = newNodeId;
+      }
+      
+      groups[nodeId] = {
+        ...groupCopy,
+        groupName: name,
+      };
+      
+      logMsg = `Renamed group from "${oldName}" to "${name}" in "${path}"`;
+    }
+    else if (type === 'preferences') {
+      data.preferences = {
+        ...data.preferences,
+        ...prefs,
+      };
+      
+      logMsg = `Updated preferences for ${Object.keys(prefs).map(pref => `"${pref}"`).join(', ')}`;
+    }
+  }
+  else if (action === 'delete') {
+    if (type === 'note') {
+      const { notes } = getPathNode(notesData, path);
+      delete notes[id];
+      
+      logMsg = `Removed note "${id}" from "${path}"`;
+    }
+    else if (type === 'group') {
+      const { groups } = getPathNode(notesData, path);
+      delete groups[id];
+      
+      logMsg = `Removed group "${id}" from "${path}"`;
+    }
+  }
+  
+  return { data, logMsg };
+};
