b7082b8a89dba3296295d925ef1f77b892fedcc4
Trevor Lemon <duhlemman.dev@gmail.com>
2022-04-27 14:09:15 -0700

    fix: Supported languages not loading
    I originally hardcoded what languages I wanted to include to minimize the final img size. That wasn't really maintainable so I'm now just including all the languages. They're still only loaded on demand though.

--- bin/prep-dist.sh
+++ bin/prep-dist.sh
@@ -52,41 +52,8 @@ rsync -avh \
   ./node_modules/prismjs/components/prism-core.min.js \
   ./dist/public/js/vendor/prism
 rsync -avh \
-  ./node_modules/prismjs/components/prism-apacheconf.min.js \
-  ./node_modules/prismjs/components/prism-applescript.min.js \
-  ./node_modules/prismjs/components/prism-arduino.min.js \
-  ./node_modules/prismjs/components/prism-bash.min.js \
-  ./node_modules/prismjs/components/prism-batch.min.js \
-  ./node_modules/prismjs/components/prism-c.min.js \
-  ./node_modules/prismjs/components/prism-clike.min.js \
-  ./node_modules/prismjs/components/prism-cpp.min.js \
-  ./node_modules/prismjs/components/prism-css.min.js \
-  ./node_modules/prismjs/components/prism-diff.min.js \
-  ./node_modules/prismjs/components/prism-docker.min.js \
-  ./node_modules/prismjs/components/prism-ejs.min.js \
-  ./node_modules/prismjs/components/prism-git.min.js \
-  ./node_modules/prismjs/components/prism-graphql.min.js \
-  ./node_modules/prismjs/components/prism-groovy.min.js \
-  ./node_modules/prismjs/components/prism-http.min.js \
-  ./node_modules/prismjs/components/prism-ini.min.js \
-  ./node_modules/prismjs/components/prism-javadoclike.min.js \
-  ./node_modules/prismjs/components/prism-javascript.min.js \
-  ./node_modules/prismjs/components/prism-jsdoc.min.js \
-  ./node_modules/prismjs/components/prism-json.min.js \
-  ./node_modules/prismjs/components/prism-json5.min.js \
-  ./node_modules/prismjs/components/prism-jsonp.min.js \
-  ./node_modules/prismjs/components/prism-jsx.min.js \
-  ./node_modules/prismjs/components/prism-log.min.js \
-  ./node_modules/prismjs/components/prism-markdown.min.js \
-  ./node_modules/prismjs/components/prism-markup.min.js \
-  ./node_modules/prismjs/components/prism-nginx.min.js \
-  ./node_modules/prismjs/components/prism-php.min.js \
-  ./node_modules/prismjs/components/prism-python.min.js \
-  ./node_modules/prismjs/components/prism-regex.min.js \
-  ./node_modules/prismjs/components/prism-sql.min.js \
-  ./node_modules/prismjs/components/prism-vim.min.js \
-  ./node_modules/prismjs/components/prism-wasm.min.js \
-  ./node_modules/prismjs/components/prism-yaml.min.js \
+  --exclude 'prism-core.min.js' \
+  ./node_modules/prismjs/components/prism-*.min.js \
   ./dist/public/js/vendor/prism/langs
 rsync -avh \
   ./node_modules/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js \
