3dfb813fc6542ecc4d0f03fd0771588880d6592b
Nox <the0neWhoKnocks@users.noreply.github.com>
2023-12-02 13:05:16 -0800

    ops: Added dev env

--- bin/prep-dist.sh
+++ bin/prep-dist.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 mkdir -p \
   ./dist \
@@ -13,50 +13,52 @@ rsync -avh \
   ./src/constants.js \
   ./dist --delete
 
-# Sync Static module files
-rsync -avh \
-  ./node_modules/dompurify/dist/purify.min.js \
-  ./node_modules/marked/marked.min.js \
-  ./dist/public/js/vendor
-
 rsync -avh \
   ./src/static/favicons \
   ./dist/public/imgs
 
-#########
-# Prism
-#########
-mkdir -p \
-  ./dist/public/css/vendor/prism/plugins \
-  ./dist/public/css/vendor/prism/themes \
-  ./dist/public/js/vendor/prism \
-  ./dist/public/js/vendor/prism/langs \
-  ./dist/public/js/vendor/prism/plugins
+# Sync Static module files (won't exist after a fresh clone)
+if [ -d ./node_modules ]; then
+  rsync -avh \
+    ./node_modules/dompurify/dist/purify.min.js \
+    ./node_modules/marked/marked.min.js \
+    ./dist/public/js/vendor
 
-rsync -avh \
-  ./node_modules/prismjs/plugins/line-numbers/prism-line-numbers.css \
-  ./node_modules/prismjs/plugins/toolbar/prism-toolbar.css \
-  ./dist/public/css/vendor/prism/plugins
-rsync -avh \
-  ./node_modules/prismjs/themes/prism-coy.css \
-  ./node_modules/prismjs/themes/prism-dark.css \
-  ./node_modules/prismjs/themes/prism-funky.css \
-  ./node_modules/prismjs/themes/prism-okaidia.css \
-  ./node_modules/prismjs/themes/prism-solarizedlight.css \
-  ./node_modules/prismjs/themes/prism-tomorrow.css \
-  ./node_modules/prismjs/themes/prism-twilight.css \
-  ./node_modules/prismjs/themes/prism.css \
-  ./dist/public/css/vendor/prism/themes
+  #########
+  # Prism
+  #########
+  mkdir -p \
+    ./dist/public/css/vendor/prism/plugins \
+    ./dist/public/css/vendor/prism/themes \
+    ./dist/public/js/vendor/prism \
+    ./dist/public/js/vendor/prism/langs \
+    ./dist/public/js/vendor/prism/plugins
 
-rsync -avh \
-  ./node_modules/prismjs/components/prism-core.min.js \
-  ./dist/public/js/vendor/prism
-rsync -avh \
-  --exclude 'prism-core.min.js' \
-  ./node_modules/prismjs/components/prism-*.min.js \
-  ./dist/public/js/vendor/prism/langs
-rsync -avh \
-  ./node_modules/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js \
-  ./node_modules/prismjs/plugins/line-numbers/prism-line-numbers.min.js \
-  ./node_modules/prismjs/plugins/toolbar/prism-toolbar.min.js \
-  ./dist/public/js/vendor/prism/plugins
+  rsync -avh \
+    ./node_modules/prismjs/plugins/line-numbers/prism-line-numbers.css \
+    ./node_modules/prismjs/plugins/toolbar/prism-toolbar.css \
+    ./dist/public/css/vendor/prism/plugins
+  rsync -avh \
+    ./node_modules/prismjs/themes/prism-coy.css \
+    ./node_modules/prismjs/themes/prism-dark.css \
+    ./node_modules/prismjs/themes/prism-funky.css \
+    ./node_modules/prismjs/themes/prism-okaidia.css \
+    ./node_modules/prismjs/themes/prism-solarizedlight.css \
+    ./node_modules/prismjs/themes/prism-tomorrow.css \
+    ./node_modules/prismjs/themes/prism-twilight.css \
+    ./node_modules/prismjs/themes/prism.css \
+    ./dist/public/css/vendor/prism/themes
+
+  rsync -avh \
+    ./node_modules/prismjs/components/prism-core.min.js \
+    ./dist/public/js/vendor/prism
+  rsync -avh \
+    --exclude 'prism-core.min.js' \
+    ./node_modules/prismjs/components/prism-*.min.js \
+    ./dist/public/js/vendor/prism/langs
+  rsync -avh \
+    ./node_modules/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js \
+    ./node_modules/prismjs/plugins/line-numbers/prism-line-numbers.min.js \
+    ./node_modules/prismjs/plugins/toolbar/prism-toolbar.min.js \
+    ./dist/public/js/vendor/prism/plugins
+fi
